1f2251ad433722c01d4cacec15344daa2ed9cc05
Slorant <sutuslorant@gmail.com>
2023-04-06 15:02:13 +0200

    ikon mar jo

--- html/bookings.php
+++ html/bookings.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <link rel="stylesheet" href="../css/bookings.css" />
-  <link rel="icon" type="image/png" href="/images/travel.png" />
+  <link rel="icon" type="image/png" href="/Travelling/images/travel.png" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
